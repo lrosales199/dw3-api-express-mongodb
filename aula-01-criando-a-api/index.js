bb43474ddd2da5db2183a255853dec077ec61331
Leonardo Rosa <leonardo.rosa_2203@outlook.com>
2026-08-12 23:04:10 -0300

    Aula 01.4 - Conectando o API ao Banco

--- aula-01-criando-a-api/index.js
+++ aula-01-criando-a-api/index.js
@@ -1,38 +1,53 @@
 // Importar o Express
 import express from "express";
 
+// Importando o Mongoose
+import mongoose from "mongoose";
+
+// Importar o Model
+import Game from "./models/Games.js";
+
+// Importar as Rotas(endpoints)
+import gameRoutes from "./routes/gameRouter.js";
+
 // Carregando express
 const app = express();
+
+// Carregando as rotas de games
+app.use("/", gameRoutes);
 
 // Configurações do Express
 app.use(express.json());
 
-// ROTA PRINCIPAL DA API
-app.get("/", (req, res) => {
-  // JSON que será retornando pela API
-  const games = [
-    {
-      title: "Fifa 2019",
-      year: 2019,
-      platform: "X-box 360",
-      price: 198,
-    },
-    {
-      title: "The Sims",
-      year: 2016,
-      platform: "PC (Windows)",
-      price: 149,
-    },
-    {
-      title: "CS GO",
-      year: 2012,
-      platform: "PC (Windows)",
-      price: 89,
-    },
-  ];
-  // Configurando o retorno do API
-  res.status(200).json(games);
-});
+// Iniciando a Conexão com o MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/apithegames");
+
+// // ROTA PRINCIPAL DA API
+// app.get("/", (req, res) => {
+//   // JSON que será retornando pela API
+//   const games = [
+//     {
+//       title: "Fifa 2019",
+//       year: 2019,
+//       platform: "X-box 360",
+//       price: 198,
+//     },
+//     {
+//       title: "The Sims",
+//       year: 2016,
+//       platform: "PC (Windows)",
+//       price: 149,
+//     },
+//     {
+//       title: "CS GO",
+//       year: 2012,
+//       platform: "PC (Windows)",
+//       price: 89,
+//     },
+//   ];
+//   // Configurando o retorno do API
+//   res.status(200).json(games);
+// });
 
 // Iniciando o servidor da API
 const port = 4000;
